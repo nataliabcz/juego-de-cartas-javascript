93beee1ca70fd0e4d0f918cba614f55c45f53669
unknown <natabarrerac56@gmail.com>
2025-07-23 12:54:40 -0500

    arreglar y contar las cartas que hay de cada valor

--- codigo/app.js
+++ codigo/app.js
@@ -1,3 +1,6 @@
+let repartio = false;
+let cartas = [];
+
 function repartir() {
     let tablero = document.getElementById("tablero");
     tablero.innerHTML = "";
@@ -5,6 +8,7 @@ function repartir() {
     for (let i = 0; i < TOTAL_CARTAS; i++) {
         let carta = document.createElement("img");
         let indice = Math.floor(Math.random() * 52) + 1;
+        cartas.push(indice);
         carta.src = "imageness/cartas/Carta" + indice + ".jpg";
         tablero.appendChild(carta);
     }
@@ -12,9 +16,32 @@ function repartir() {
     audio.play().then(() => {
         window.alert("se han repartido las 10 cartas");
     });
-
+    repartio = true;
 }
 
 function verificar() {
-    window.alert("mañana chequearemos las cartas");
+    if (!repartio) {
+        window.alert("No se han repartido las cartas");
+    }
+    else {
+        //iniciar los contadores en 0
+        contadores = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        //recorrer la lista de las cartas 
+        contadores = new Array(13).fill(0);
+        for (let i = 0; i < cartas.length; i++) {
+            let posicion = cartas[i] % 13;
+            if (posicion == 0) {
+                posicion = 12;
+            }
+            else{ posicion--;
+            }
+            contadores [posicion]++;
+
+        }
+        nombreCartas=["As","2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+        //recorrer los contadores
+        for (let i = 0; i <contadores.length; i++){
+            window.alert("Hay " + contadores[i] + " Cartas de " + nombreCartas[i]);
+        }
+    }
 }
